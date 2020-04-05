edb0e2c7028e1aed9a08f1a370629b81cfdee796
lorenzovolpi <yoyopro@gmail.com>
2020-04-05 16:12:31 +0200

    day02 solved

--- source/day02/happy_number.cpp
+++ source/day02/happy_number.cpp
@@ -1,6 +1,37 @@
+#include <iostream>
+#include <math.h>
+#include <set>
+
 class Solution {
+private:
+    int digit_sqr_sum(int n) {
+        int sum = 0;
+        while(n > 0) {
+            sum += std::pow(n % 10, 2);
+            n /= 10;
+        }
+        return sum;
+    }
+
 public:
     bool isHappy(int n) {
-        
+        if(n == 1) return true;
+
+        std::set<int> seen;
+        seen.insert(n);
+        while(true) {
+            n = digit_sqr_sum(n);
+            if (n == 1) return true;
+            else if(seen.find(n) != seen.end()) return false;
+            else seen.insert(n);
+        }
     }
 };
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    Solution* sol = new Solution();
+    std::cout << (sol->isHappy(n) ? "true" : "false") << std::endl;
+}
