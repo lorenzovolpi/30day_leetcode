271905e94392955bf636b124802f1ce07f3625c7
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2020-04-13 02:14:02 +0200

    day12 started

---
+++ source/30daychallenge/12_last_stone_weight.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+	sort(stones.begin(), stones.end());
+		
+    }
+};
+
+
+int main() {
+
+}
+
